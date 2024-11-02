b769bc4a504729bb3f70176b7b8b72ef148de7a4
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2024-11-01 23:50:29 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Button } from "@varugasu/reproduction-lib/components/Button";
+import {
+  AcUnitIcon,
+  AccessibilityIcon,
+} from "@varugasu/reproduction-lib/components/icons";
+import { PrimaryButton } from "@varugasu/reproduction-lib/custom-components/button";
+import {
+  SCREEN_HEIGHT,
+  SCREEN_WIDTH,
+} from "@varugasu/reproduction-lib/constants";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+      <AcUnitIcon />
+      <AccessibilityIcon />
+      <PrimaryButton>test</PrimaryButton>
+      <p>{SCREEN_HEIGHT}</p>
+      <p>{SCREEN_WIDTH}</p>
+    </div>
+  );
+}
+
+export default App;
